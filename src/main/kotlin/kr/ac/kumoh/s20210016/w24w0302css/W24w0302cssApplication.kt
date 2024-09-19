d3ec06ab3f3b60ed404fe4c5e150950c4dbc9c0c
seungjae915 <seungjae915@naver.com>
2024-09-19 14:28:00 +0900

    🎉 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20210016/w24w0302css/W24w0302cssApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210016.w24w0302css
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w0302cssApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w0302cssApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210016/w24w0302css/W24w0302cssApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210016.w24w0302css
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w0302cssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
